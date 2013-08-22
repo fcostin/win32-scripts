df7c85fa0a35e265511cb9f4472e82f1af211ca1
Reuben Fletcher-Costin <reuben.fletchercostin@gmail.com>
2013-08-22 12:33:45 +1000

    use argparse and logging, improve readme

---
+++ README.markdown
@@ -0,0 +1,19 @@
+win32_chowntree
+===============
+
+usage
+-----
+
+    C:\>python win32_chowntree.py --help
+    usage: win32_chowntree.py [-h] [-u USERNAME] DIR
+
+    recursively enable all permissions for files in target_dir
+
+    positional arguments:
+      DIR                   target directory
+
+    optional arguments:
+      -h, --help            show this help message and exit
+      -u USERNAME, --user_account_name USERNAME
+                            account name. Default is "Everyone".
+
--- win32_chowntree.py
+++ win32_chowntree.py
@@ -5,27 +5,28 @@ file access permissions are replaced, allowing everyone all access
 
 only tested on windows 7 ultimate N
 
-edit the root path in main() to specify the target
-
 based on Tim Golden's incredibly helpful example here:
     http://timgolden.me.uk/python/win32_how_do_i/add-security-to-a-file.html
 """
 
+import sys
 import os
-import win32api
 import win32security
 import ntsecuritycon as con
+import argparse
+import logging
+
 
 def fix_access(file_name, acc_name):
     """
     replace access permissions for file with FILE_ALL_ACCESS for acc_name
     """
-    print 'fixing access for "%s"' % file_name
+    logging.info('fixing access for "%s"' % file_name)
     sd = win32security.GetFileSecurity(file_name, win32security.DACL_SECURITY_INFORMATION)
     dacl = win32security.ACL()
     dacl.AddAccessAllowedAce(win32security.ACL_REVISION, con.FILE_ALL_ACCESS, acc_name)
     sd.SetSecurityDescriptorDacl(1, dacl, 0)
-    hresult = win32security.SetFileSecurity(file_name, win32security.DACL_SECURITY_INFORMATION, sd)
+    win32security.SetFileSecurity(file_name, win32security.DACL_SECURITY_INFORMATION, sd)
 
 def fix_access_subtree(root, acc_name):
     """
@@ -41,9 +42,23 @@ def get_acc_name(name):
     acc_name, _, _ = win32security.LookupAccountName('', name)
     return acc_name
 
-def main():
-    root = 'D:\\cygwin_packages' # change this ...
-    fix_access_subtree(root, get_acc_name('Everyone'))
+def parse_args(args):
+    p = argparse.ArgumentParser(description='recursively enable all permissions for files in target_dir')
+    p.add_argument('root', metavar='DIR', help='target directory')
+    p.add_argument('-u', '--user_account_name', metavar='USERNAME', dest='account_name', default='Everyone', help='account name. Default is "Everyone".')
+    return p.parse_args()
+
+def setup_logging():
+    logging.basicConfig(level=logging.DEBUG,
+        format='%(asctime)s %(levelname)s %(message)s')
+
+def main(argv=None):
+    setup_logging()
+    if argv is None:
+        argv = sys.argv[1:]
+    args = parse_args(argv)
+    fix_access_subtree(args.root, get_acc_name(args.account_name))
 
 if __name__ == '__main__':
     main()
+
